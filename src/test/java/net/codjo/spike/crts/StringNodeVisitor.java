23c7b163b8ac0adb367236ef5856d93896fc6946
gonnot <gonnot@yahoo.com>
2012-08-08 23:59:32 +0200

    Add copyright notice.

--- src/test/java/net/codjo/spike/crts/StringNodeVisitor.java
+++ src/test/java/net/codjo/spike/crts/StringNodeVisitor.java
@@ -1,9 +1,29 @@
+/*
+ * codjo (Prototype)
+ * =================
+ *
+ *    Copyright (C) 2012, $YEAR$ by codjo.net
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *    implied. See the License for the specific language governing permissions
+ *    and limitations under the License.
+ */
+
 package net.codjo.spike.crts;
 
 public class StringNodeVisitor implements NodeVisitor<String> {
     private static final String BRANCH = " *-- ";
     private static final String INDENT = "     ";
     private int level = 0;
+
 
     public String visit(Node node) {
         StringBuilder builder = new StringBuilder();
