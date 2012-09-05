64df0bb33fabb63d8f52b22b1818ad67a5b14da8
gonnot <gonnot@yahoo.com>
2012-09-05 22:41:58 +0200

    use case 'XSD generator' - prepare next step - Now we can test XSD by validating XML file (More flexible)

--- src/test/java/net/codjo/spike/crts/sample/schema/XsdWriterTest.java
+++ src/test/java/net/codjo/spike/crts/sample/schema/XsdWriterTest.java
@@ -19,15 +19,32 @@
 
 package net.codjo.spike.crts.sample.schema;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import net.codjo.spike.crts.kernel.RuleEngine;
 import net.codjo.test.common.XmlUtil;
 import net.codjo.test.common.fixture.DirectoryFixture;
 import net.codjo.util.file.FileUtil;
+import org.hamcrest.Description;
+import org.intellij.lang.annotations.Language;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.internal.matchers.TypeSafeMatcher;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import static net.codjo.spike.crts.api.definition.DefinitionBuilder.node;
+import static net.codjo.test.common.matcher.JUnitMatchers.*;
 /**
  *
  */
@@ -56,6 +73,42 @@ public class XsdWriterTest {
 
         XmlUtil.assertEquivalent(FileUtil.loadContent(getClass().getResource("XsdWriter-oneSimpleTag.xsd")),
                                  FileUtil.loadContent(xsdFile));
+    }
+
+
+    @Test
+    public void testDefineOneTag() throws Exception {
+        engine.declare(node("file-assert"));
+        engine.start();
+
+        xsdWriter.createXsdFile(engine.getRootNode(), xsdFile);
+
+        assertXmlCompliant(xml("<release-test/>"));
+
+        assertXmlCompliant(xml("<release-test>\n"
+                               + "    <file-assert/>\n"
+                               + "</release-test>"));
+
+        assertXmlCompliant(xml("<release-test>\n"
+                               + "    <file-assert/>\n"
+                               + "    <file-assert/>\n"
+                               + "</release-test>"));
+
+        assertXmlNotCompliant(xml("<release-test>\n"
+                                  + "    <file-assert>\n"
+                                  + "       <file-assert/>\n"
+                                  + "    </file-assert>"
+                                  + "</release-test>"));
+    }
+
+
+    private void assertXmlNotCompliant(String xml) throws SAXException, ParserConfigurationException, IOException {
+        assertThat(xml, is(not(xsdCompliant(xsdFile))));
+    }
+
+
+    private void assertXmlCompliant(String xml) throws Exception {
+        assertThat(xml, is(xsdCompliant(xsdFile)));
     }
 
 
@@ -110,5 +163,55 @@ public class XsdWriterTest {
     @After
     public void tearDown() throws Exception {
         directory.doTearDown();
+    }
+
+
+    private static String xml(@Language("XML") String xml) throws SAXException, ParserConfigurationException, IOException {
+        return xml;
+    }
+
+
+    private static IsXsdCompliant xsdCompliant(File xsdFile) {
+        return new IsXsdCompliant(xsdFile);
+    }
+
+
+    public static class IsXsdCompliant extends TypeSafeMatcher<String> {
+        private File xsdFile;
+        private Exception failingValidation;
+
+
+        public IsXsdCompliant(File xsdFile) {
+            this.xsdFile = xsdFile;
+        }
+
+
+        @Override
+        public boolean matchesSafely(String xml) {
+            try {
+                SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+                Schema schema = factory.newSchema(xsdFile);
+                Validator validator = schema.newValidator();
+
+                DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                Document document = parser.parse(new InputSource(new StringReader(xml)));
+                validator.validate(new DOMSource(document));
+                return true;
+            }
+            catch (Exception e) {
+                this.failingValidation = e;
+                return false;
+            }
+        }
+
+
+        public void describeTo(Description description) {
+            if (failingValidation != null) {
+                description.appendText("Not compliant with XSD : " + failingValidation.getLocalizedMessage());
+            }
+            else {
+                description.appendText("Compliant with XSD");
+            }
+        }
     }
 }
